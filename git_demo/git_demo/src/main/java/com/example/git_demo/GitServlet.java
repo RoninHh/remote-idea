dd8d35673fc8e48dbceedf4ed06b86a042f0cba3
hhy <hhy@qq.com>
2023-06-13 14:08:08 +0800

    dev修改了doGet

--- git_demo/git_demo/src/main/java/com/example/git_demo/GitServlet.java
+++ git_demo/git_demo/src/main/java/com/example/git_demo/GitServlet.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class GitServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("dev修改");
         super.doGet(req, resp);
     }
 
